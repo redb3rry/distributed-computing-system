6f346e5827a44f0c3632258ad0f210bcfbdc7ec8
kczaj <kubapcz@gmail.com>
2021-05-31 19:45:20 +0200

    Adding abstract class

--- worker.py
+++ worker.py
@@ -1,19 +1,21 @@
 import asyncio
 import websockets
+import abc
 
 
-def do_work(task):
-    return 0
+class Worker(abc.ABC):
+    @abc.abstractmethod
+    def do_work(self, task):
+        return 0
 
+    def __init__(self, uri):
+        self.uri = uri
 
-async def handle_connection():
-    uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
+    async def handle_connection(self):
+        async with websockets.connect(self.uri) as websocket:
+            task = await websocket.recv()
+            result = self.do_work(task)
+            await websocket.send(result)
 
-        task = await websocket.recv()
-        result = do_work(task)
-        await websocket.send(result)
-
-
-def run():
-    asyncio.get_event_loop().run_until_complete(handle_connection())
+    def run(self):
+        asyncio.get_event_loop().run_until_complete(self.handle_connection())
--- worker_implementation.py
+++ worker_implementation.py
@@ -1,3 +1,13 @@
-import worker
+from worker import Worker
 
-worker.run()
+
+class WorkerImplementation(Worker):
+
+    def __init__(self, uri):
+        super(WorkerImplementation, self).__init__(uri)
+
+    def do_work(self, task):
+        return 0
+
+
+WorkerImplementation("ws://localhost:8765").run()
